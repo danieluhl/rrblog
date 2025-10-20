1a37a6aeae9d54d6c2f85982323bf78748839c71
Dan Uhl <danielruhl@gmail.com>
2025-10-20 08:46:02 -0400

    adds wrangler files for cloudflare

--- app/routes/post.tsx
+++ app/routes/post.tsx
@@ -3,18 +3,18 @@ import { getPostBySlug } from "../../scripts/get-posts";
 import type { Route } from "./+types/post";
 
 export async function loader({ params }: Route.LoaderArgs) {
-  let post = getPostBySlug(params.slug);
+  const post = getPostBySlug(params.slug);
   return post;
 }
 
 export function meta({ loaderData }: Route.MetaArgs) {
   return [
-    { title: loaderData.title },
-    { name: "description", content: loaderData.description },
+    { title: loaderData?.title },
+    { name: "description", content: loaderData?.description },
   ];
 }
 
-export default function Post({ params, loaderData }: Route.ComponentProps) {
+export default function Post({ loaderData }: Route.ComponentProps) {
   return (
     <div>
       <h1>{loaderData.title}</h1>
